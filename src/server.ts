d62688b6e707b6eb449eb208f02cad9ea8566aa4
Rui Neto <ruineto11@gmail.com>
2022-05-03 19:56:33 -0300

    docs: removing files

---
+++ src/server.ts
@@ -0,0 +1,8 @@
+import app from "./app.js";
+import dotenv from 'dotenv';
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Running on port ${PORT}`);
+});
